2aed4b9da1953eb046dd7afd3f8b98c718951d06
drosso <drosso@mpf.gov.ar>
2022-04-06 14:46:33 -0300

    Eliminar desde grilla

--- Pages/Profesores/Listado.cshtml.cs
+++ Pages/Profesores/Listado.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyPage1.Servicios;
+using ProyPage1.Model;
 
 namespace ProyPage1.Pages.Profesores
 {
     public class ListadoModel : PageModel
     {
+        [BindProperty]
+        public List<Profesor> Profesores{get;set;}
+         private IProfesorServicio _profServicio;
+
+        public ListadoModel(IProfesorServicio profServicio){
+            _profServicio=profServicio;
+        }
         public void OnGet()
         {
+            Profesores=_profServicio.GetAll().ToList();
+        }
+
+        public void OnGetDelete(int legajoborrar){
+            var ProfesorBuscado=_profServicio.GetAll().Where(x=> x.Legajo==legajoborrar).First();
+            _profServicio.Eliminar(ProfesorBuscado);
+            Profesores=_profServicio.GetAll().ToList();
         }
     }
 }
