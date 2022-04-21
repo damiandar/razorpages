e1661aa2525af0027f7e56578df0af2f1026d114
drosso <drosso@mpf.gov.ar>
2022-04-21 13:01:33 -0300

    imagen etiqueta

--- Startup.cs
+++ Startup.cs
@@ -64,7 +64,7 @@ namespace ProyPage1
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
             //app.UseHttpsRedirection();
