06bc20226148356d50fd3cf3d4a19cce61879dfa
drosso <drosso@mpf.gov.ar>
2022-04-21 11:04:21 -0300

    Dropdownlist

--- Pages/Profesores/Editar.cshtml.cs
+++ Pages/Profesores/Editar.cshtml.cs
@@ -6,21 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProyPage1.Model;
 using ProyPage1.Servicios;
+ 
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ProyPage1.Pages.Profesores
 {
     public class EditarModel : PageModel
     {
         private IProfesorServicio _profService; 
+        private IMateriaServicio _mateService;
 
-        public EditarModel(IProfesorServicio profServicio){
+        public EditarModel(IProfesorServicio profServicio,IMateriaServicio mateServicio){
             _profService=profServicio;
+            _mateService=mateServicio;
         }
     
         [BindProperty]
         public Profesor profesor {get;set;}
         public void OnGet(int leg)
         {
+            var materias=_mateService.GetAll();
+            ViewData["Materias"] =new SelectList(materias,"Id", "Descripcion");
+            ViewData["MateriasDirecto"]=materias;
+            
+
             profesor=_profService.GetAll().Where(x=>x.Legajo==leg).First();
         }
 
